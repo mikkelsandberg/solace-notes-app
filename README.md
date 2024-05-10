b919b8b870d16d2c7b0d42822980685e1d8eb164
Mikkel Sandberg <mikkel@littlebook.studio>
2024-05-10 11:10:37 -0700

    adding login page UI

--- README.md
+++ README.md
@@ -3,7 +3,15 @@ Notes web app for the Solace technical assignment.
 
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
-## Getting Started
+## Notes
+
+- The live version of this app can be found at: https://solace-notes-app-smoky.vercel.app/
+- The authentication backend is from Vercel: https://vercel.com/templates/next.js/prisma-postgres-auth-starter
+- The UI for the login screen is from MUI: https://github.com/mui/material-ui/tree/v5.15.17/docs/data/material/getting-started/templates/sign-in
+- The UI for the notes is based on MUI as well: https://github.com/mui/material-ui/tree/v5.15.17/docs/data/material/getting-started/templates/dashboard
+- The biggest challenge was setting up the authentication flow with next-auth and getting everything deployed on Vercel, as these systems were new to me.
+
+## Local Development
 
 First, run the development server:
 
@@ -19,21 +27,4 @@ bun dev
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
 This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,31 +1,87 @@
-import { LoginForm } from "@/app/loginForm";
-import { SubmitButton } from "@/app/submitButton";
-import { signIn } from "@/auth";
-import { redirect } from "next/navigation";
+'use client';
 
-export default function Login() {
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import { getSession } from 'next-auth/react';
+import { useState } from 'react';
+import { handleLogin } from './loginUtils';
+
+export default function LogIn() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const handleSubmit = async (formData: FormData) => {
+    await getSession();
+
+    try {
+      setError(null);
+      setLoading(true);
+  
+      await handleLogin(formData);
+  
+      setLoading(false);
+    } catch (error) {
+      setError('Incorrect email or password. Please try again.');
+      setLoading(false);
+    }
+  };
+
   return (
-    <LoginForm
-      action={async (formData: FormData) => {
-        'use server';
-        
-        const result = await signIn('credentials', {
-          email: formData.get('email') as string,
-          password: formData.get('password') as string,
-          redirect: false,
-        });
-
-        console.log('result', result);
-
-        if (result) {
-          redirect('/notes');
-        } else {
-          console.log('Sign in failed');
-          return;
-        }
-      }}
-    >
-      <SubmitButton>Sign in</SubmitButton>
-    </LoginForm>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Login
+        </Typography>
+        <Box sx={{ mt: 1 }}>
+          <form autoComplete="off" action={handleSubmit}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+            />
+            {error && <Typography color="error">{error}</Typography>}
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              disabled={loading}
+            >
+              {loading ? 'Loading' : 'Login' }
+            </Button>
+          </form>
+        </Box>
+      </Box>
+    </Container>
   );
 }
